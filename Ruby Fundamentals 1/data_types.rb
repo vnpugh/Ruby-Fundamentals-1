22d7fe6a1607568bf2bb4cd0f36f297e80224d91
Velvet Pugh <48695329+vnpugh@users.noreply.github.com>
2024-02-05 18:50:57 -0600

    Add files via upload

---
+++ Ruby Fundamentals 1/data_types.rb
@@ -0,0 +1,20 @@
+# Ruby Primitive Data Types
+
+# 1. Strings
+# Single-quoted strings are simpler and best used when you need a string exactly as it is.
+'Hello Ruby World'
+# Double-quoted strings are more flexible and allow for escape sequences and string interpolation. 
+# Escape sequences are special characters that are preceded by a backslash (\) to denote a special meaning. 
+# For example, \n represents a newline character, \t represents a tab, and \\ represents a backslash.
+"Hello Ruby World"
+name = "Ruby"
+puts "Hello, #{name}!\nWelcome to programming!"
+
+# 2. Numbers
+# Integer
+42
+# Float
+42.3
+
+# 3. Boolean
+
