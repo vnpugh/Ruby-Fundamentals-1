af807591388587175d33c88768bf609b0e548163
Velvet Pugh <vnp19@yahoo.com>
2024-02-05 19:28:55 -0600

    adding files

--- Ruby Fundamentals 1/data_types.rb
+++ Ruby Fundamentals 1/data_types.rb
@@ -36,3 +36,35 @@ puts array[0]   # => 10
 puts array[2]   # => 30
 puts array[-1]  # => 50 (last element)
 
+#Modifying Arrays
+#Adding Elements using push, <<(shovel), or insert.
+array = [] (initialize empty array)
+1. array.push(1) -> appends the object to the end of the array
+2. array << 2 (using the shovel operator) -> also appends the object to the end of the array
+3. array.insert(1, 1.5) ->  (more versatile) adds new elements at the specified index # => [1, 1.5, 2]
+
+# 2. Hashes: associative arrays or dictionaries, collections of key-value pairs. 
+# they store data in a structured way, but they are not the same as objects created from custom classes.
+* Key-Value Pairs: Each entry in a hash consists of a key and a value. The key is used to retrieve the corresponding value.
+* Flexible Keys and Values: Hash keys and values can be objects of any type, including numbers, strings, arrays.
+
+# Creating a hash in Ruby:
+my_hash = {
+    'name' => 'Alice',
+    'age' => 30,
+    'is_student' => true
+  }
+Access or modify hash values using their keys:
+  
+  puts my_hash['name'] # => Alice
+  my_hash['age'] = 31 # Update the age
+
+# Newer syntax:
+
+  student = {
+  name: 'Bob',
+  grade: 'A'
+  }
+
+  # 3. Nil : a value that represents the concept of "nothing" or "no value".
+  
