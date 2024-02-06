fe8c1f55b5be28807e142c7d15a47b21ff24f70f
Velvet Pugh <vnp19@yahoo.com>
2024-02-05 19:14:08 -0600

    second commit

--- Ruby Fundamentals 1/data_types.rb
+++ Ruby Fundamentals 1/data_types.rb
@@ -17,4 +17,22 @@ puts "Hello, #{name}!\nWelcome to programming!"
 42.3
 
 # 3. Boolean
+# used to track truthiness or falsity of conditions.
+
+# Complex Data Types
+
+# 1. Arrays: an ordered list of elements used to store collections of items. 
+#Create using Square Brackets:
+numbers = [1, 2, 3, 4, 5]
+words = ['hello', 'world']
+mixed = [1, 'two', 3.0, [4, 'five']]
+#Create using the Array.new Method: specified size and default value.
+empty_array = Array.new(3)  # => [nil, nil, nil]
+zeros = Array.new(3, 0)     # => [0, 0, 0]
+
+# Accessing Elements: Ruby arrays are zero-indexed, meaning the first element is at index 0.
+array = [10, 20, 30, 40, 50]
+puts array[0]   # => 10
+puts array[2]   # => 30
+puts array[-1]  # => 50 (last element)
 
